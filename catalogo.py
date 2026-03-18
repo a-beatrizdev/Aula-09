88710a44568f1d5ba4c909eebb667b2816343090
Alana <alanabeatriztobias>
2026-03-18 16:31:25 -0300

    Adicionando Funções no CRUD

--- catalogo.py
+++ catalogo.py
@@ -26,3 +26,40 @@ class Filme(Base):
     def __repr__(self):
         return f"Filme = id={self.id} - nome={self.titulo} - genero={self.genero}"
 
+
+
+#Conexão com o banco
+engine = create_engine("sqlite:///catalogo_filmes.db")
+
+#Criar as tabelas
+Base.metadata.create_all(engine)
+
+#Crio fábrica de sessões no banco
+Session = sessionmaker(bind=engine)
+
+#Funções do CRUD
+def cadastrar_filme():
+    print(f"\n--- CADASTRAR FILMES ---\n")
+    titulo = input("Digite o nome do filme: ")
+    genero = input("Digite o gênero do filme: ")
+    ano = int(input("Digite o ano de lançamento do filme: "))
+    nota = float(input("Digite a nota do filme: "))
+
+    with Session() as session:
+        try:
+            filmes_existentes = session.query(Filme).filter_by(titulo=titulo, ano_lancamento=ano).first()
+            if filmes_existentes == None:
+                novo_filme = Filme(titulo, genero, ano, nota)
+                session.add(novo_filme) 
+                session.commit()
+            else:
+                print(f"Já existe um filme cadastrado com esse título e ano")
+        except Exception as erro:
+            session.rollback()
+            print(f"OCorreu um erro {erro}")
+
+#Chamar a função
+cadastrar_filme()
+
+#Criar a função listar e função deletar
+
